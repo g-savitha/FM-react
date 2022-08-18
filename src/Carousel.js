1d02af8c5602d75c85d41d4216a7456eb49ae7c7
Savitha Gollamudi <savitha_gollamudi@epam.com>
2022-08-18 16:36:08 +0530

    initial commit

---
+++ src/Carousel.js
@@ -0,0 +1,43 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+import React, { Component } from "react";
+
+class Carousel extends Component {
+  state = {
+    active: 0,
+  };
+
+  static defaultProps = {
+    images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
+  };
+
+  handleIndexClick = (e) => {
+    this.setState({
+      active: +e.target.dataset.index,
+    });
+  };
+
+  render() {
+    const { active } = this.state;
+    const { images } = this.props;
+    return (
+      <div className="carousel">
+        <img src={images[active]} alt="animal" />
+        <div className="carousel-smaller">
+          {images.map((photo, index) => (
+            // eslint-disable-next-line jsx-a11y/click-events-have-key-events
+            <img
+              onClick={this.handleIndexClick}
+              key={photo}
+              src={photo}
+              data-index={index}
+              className={index === active ? "active" : ""}
+              alt="animal thumbnail"
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
